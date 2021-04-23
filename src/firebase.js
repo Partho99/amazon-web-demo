b0da6ef4c85f6524ff2d713ce20cd59ff9e04f6e
Partho Das <partho.swe@gmail.com>
2021-04-23 20:11:58 +0600

    navbar design almost done

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyAz8w_5nN_lQHzoIvnuKxD8AO8aSKkPt9E",
+    authDomain: "web-demo-675ef.firebaseapp.com",
+    projectId: "web-demo-675ef",
+    storageBucket: "web-demo-675ef.appspot.com",
+    messagingSenderId: "445298574920",
+    appId: "1:445298574920:web:6e0d170d856b65386bfa7d",
+    measurementId: "G-ZD47JV3ZFT"
+  };
