5275a7e0d161a5f5010b5e3a619e5ddfe7327435
Partho Das <partho.swe@gmail.com>
2021-04-25 01:09:58 +0600

    added context API for global state management

---
+++ src/StateProvider.jsx
@@ -0,0 +1,14 @@
+import React, { createContext, useContext, useReducer } from "react";
+
+// Prepares the Data layer
+export const StateContext = createContext();
+
+// Wrap our app and provide the Data Layer
+export const StateProvider = ({ reducer, initialState, children }) => (
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateContext.Provider>
+);
+
+// pull information from the Data Layer
+export const useStateValue = () => useContext(StateContext);
