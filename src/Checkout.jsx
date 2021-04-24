ae4add55b2f2c76d851bea1adb9a7f4bb123d64e
Partho Das <partho.swe@gmail.com>
2021-04-25 01:12:02 +0600

    add items count and sum of all items using context api and checkout component added

---
+++ src/Checkout.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./Checkout.css";
+import Subtotal from "./Subtotal";
+const Checkout = () => {
+  return (
+    <div className="checkout">
+      <div className="checkout__left">
+        <img
+          className="checkout__ad"
+          src="https://images-eu.ssl-images-amazon.com/images/G/31/img18/Coupons/ART/Diwali-18/stripe_pc._CB483582784_.jpg"
+          alt="offer"
+        />
+        <div>
+          <h2 className="checkout__title">Your shopping basket</h2>
+        </div>
+      </div>
+      <div className="checkout__right">
+        <Subtotal />
+      </div>
+    </div>
+  );
+};
+
+export default Checkout;
